59b12b1f9641a164c682274564ac104cec549327
itsyhagop <yhagopessoa@gmail.com>
2024-11-23 16:36:20 -0300

    23/11/2024

--- Script.js
+++ Script.js
@@ -2,38 +2,38 @@ function aumenta() {
     let x = parseFloat(document.getElementById("numero 1").value)
     let y = parseFloat(document.getElementById("numero 2").value) 
     
-    total = x + y
-      document.getElementById("resultado").innerHTML = "Total: " + total.toFixed(2);
+    resultado = x + y
+      document.getElementById("resultado").innerHTML = "Total: " + resultado.toFixed(2);
     
     clear()
     }
       
    
 function multiplicação(){
-let t = parseFloat(document.getElementById("numero 1").value) 
-let o = parseFloat(document.getElementById("numero 2").value) 
-    oi = t * o
-    document.getElementById("resultado").innerHTML = "Total: " + oi.toFixed(2);
+let x = parseFloat(document.getElementById("numero 1").value) 
+let y = parseFloat(document.getElementById("numero 2").value) 
+    resultado = x * y
+    document.getElementById("resultado").innerHTML = "Total: " + resultado.toFixed(2);
 clear()
 }
 
 function diminuir() {
-let p = parseFloat(document.getElementById("numero 1").value)   
-let i = parseFloat(document.getElementById("numero 2").value) 
+let x = parseFloat(document.getElementById("numero 1").value)   
+let y = parseFloat(document.getElementById("numero 2").value) 
 
-po = p - i
-    document.getElementById("resultado").innerHTML = "Total: " + po.toFixed(2);
+resultado = x - y
+    document.getElementById("resultado").innerHTML = "Total: " + resultado.toFixed(2);
 clear()
 }
 
 
 
 function dividr() {
-let u = parseFloat(document.getElementById("numero 1").value)   
-let r = parseFloat(document.getElementById("numero 2").value) 
+let x = parseFloat(document.getElementById("numero 1").value)   
+let y = parseFloat(document.getElementById("numero 2").value) 
 
-op = u / r
-    document.getElementById("resultado").innerHTML = "Total: " + op.toFixed(2)
+resultado = x / y
+    document.getElementById("resultado").innerHTML = "Total: " + resultado.toFixed(2)
 
     clear()
     
